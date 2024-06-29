fa60fb1fd39aa17d8fb75c98470b81ab75f848fb
El.ghoul Alaa <ae7_07@outlook.com>
2024-06-29 17:21:15 +0300

    about update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from '../assets/about.png'
+import homeLogo from '../assets/about.jpeg'
 import Particle from '../components/Particle';
 import About from '../components/Home/About';
 import Type from '../components/Home/Type';
